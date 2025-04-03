c219378799c78bdb3aeb43e5b8742b471c466791
Srishti <srishtisethi31@gmail.com>
2025-04-03 08:53:44 +0530

    index.js added in models

---
+++ server/src/models/index.js
@@ -0,0 +1,9 @@
+import {User} from "./User.models.js"
+import {Farmer} from "./Farmer.models.js"
+import {Spoc} from "./Spoc.models.js"
+import {PowerPlant} from "./Powerplant.models.js"
+import { Order } from "./Orders.models.js"
+import { Request } from "./Request.models.js"
+import { Otp } from "./Otp.models.js"
+
+export {User , Farmer , Spoc , PowerPlant  , Order  , Request , Otp} 
